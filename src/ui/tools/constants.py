9b0ca0b8bf0f3920aeadcdacc878425f82260693
Anton Bogomolov <ashteriel@ya.ru>
2025-01-08 11:13:42 +0200

    add new constants

--- src/ui/tools/constants.py
+++ src/ui/tools/constants.py
@@ -7,6 +7,8 @@ class Constants(object):
     try:
         login = os.getenv("AUTH_LOGIN")
         password = os.getenv("AUTH_PASSWORD")
+        base_url = os.getenv("BASE_URL")
+        environment = os.getenv("ENV")
     except KeyError:
         raise EnvironmentError(
             "AUTH_LOGIN и/или AUTH_PASSWORD должны быть указаны"
